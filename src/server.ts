32f1895ef6aed018f35463bdcbd385d184b90ae2
FredMSJ <fmdsj13@gmail.com>
2021-04-21 21:24:14 -0300

    segundo dia terminado

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,34 @@
 import express, { response } from "express";
 
+import "./database";
+import {routes} from "./routes"
+
 const app = express();
 
-/**
- * GET = Buscas
- * POSTS = Criação
- * PUT = Alteração
- * DELETE = Deletar
- * PATCH = Alterar uma informação específica
- */
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW #5",
-  });
-});
-
-app.post("/",(request, response) => {
-  return response.json({
-    message: "Usuário cadastrado com sucesso!",
-  });
-});
+app.use(routes);
 
 app.listen(3333,() => console.log("Server rodando na porta 3333"));
+
+
+// /**
+//  * GET = Buscas
+//  * POSTS = Criação
+//  * PUT = Alteração
+//  * DELETE = Deletar
+//  * PATCH = Alterar uma informação específica
+//  */
+
+// app.get("/", (request, response) => {
+//   return response.json({
+//     message: "Olá NLW #5",
+//   });
+// });
+
+// app.post("/",(request, response) => {
+//   return response.json({
+//     message: "Usuário cadastrado com sucesso!",
+//   });
+// });
 
