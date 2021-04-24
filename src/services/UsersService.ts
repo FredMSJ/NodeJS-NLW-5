519a599c0885763eb2402f9356e1f54df2160fb2
FredMSJ <fmdsj13@gmail.com>
2021-04-24 18:29:48 -0300

    Terceiro dia

---
+++ src/services/UsersService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor(){
+    this.usersRepository = getCustomRepository(UsersRepository)
+
+  }
+  async create(email: string) {
+     //Verificar se o usuario existe
+    const userExists = await this.usersRepository.findOne({
+      email
+    })
+    //Se não existir, salvar no DB
+    if(userExists){
+      return userExists;
+    }
+
+    const user = this.usersRepository.create({
+      email
+    });
+
+    await this.usersRepository.save(user);
+
+    //Se existir, retornar o user
+    return user;
+
+  }
+}
+
+export { UsersService }
